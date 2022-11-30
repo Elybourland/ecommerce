2a7fe706806dd48908765b535871404c9d10dec5
Elybourland <JEBourland@yahoo.com>
2022-11-30 09:43:47 -0800

    success page

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -99,7 +99,10 @@ export const StateContext = ({ children }) => {
         decQty,
         onAdd,
         toggleCartItemQuantity,
-        onRemove
+        onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities
       }}
     >
       {children}
